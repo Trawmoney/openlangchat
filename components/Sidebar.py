4c610e2015a76cec361ac37259c0a1e67d0ac066
Neuromancer <79415352+Trawmoney@users.noreply.github.com>
2023-09-29 06:41:58 -0400

    Update Sidebar.py

--- components/Sidebar.py
+++ components/Sidebar.py
@@ -94,7 +94,7 @@ st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
                 del st.session_state["api_key"]
                 st.experimental_rerun()
         st.markdown(
-            "[View the source code](https://github.com/alexanderatallah/openrouter-streamlit)"
+            "[View the source code](https://github.com/Trawmoney/openlangchat)"
         )
         # st.markdown(
         #     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/alexanderatallah/openrouter-streamlit?quickstart=1)"
